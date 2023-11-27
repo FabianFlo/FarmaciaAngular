02b4ff577f721779b8d05971c6c091f1bec1e977
Faflo <fa.flores@duocuc.cl>
2023-11-27 12:17:20 -0300

    fun(deleteprod){questprofe}

--- src/app/productos/listar-productos/listar-productos.page.ts
+++ src/app/productos/listar-productos/listar-productos.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { doc, deleteDoc } from 'firebase/firestore';
 
 @Component({
   selector: 'app-listar-productos',
@@ -8,11 +10,20 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 })
 export class ListarProductosPage implements OnInit {
   productos: any[];
+  usuarioActual: any;
 
-  constructor(private firestore: AngularFirestore) { }
+  constructor(private firestore: AngularFirestore,
+      private auth: AngularFireAuth) { }
 
   ngOnInit() {
     this.obtenerProductos();
+    this.obtenerUsuarioActual();
+  }
+
+  obtenerUsuarioActual() {
+    this.auth.authState.subscribe((user) => {
+      this.usuarioActual = user;
+    });
   }
 
   obtenerProductos() {
@@ -24,4 +35,29 @@ export class ListarProductosPage implements OnInit {
       });
   }
 
+  puedeEliminarProducto(producto: any): boolean {
+    return this.usuarioActual && this.usuarioActual.email === 'si@si.si';
+  }
+  
+  async eliminarProducto(id: string) {
+    console.log('Intentando eliminar producto con ID:', id);
+    // Asegúrate de verificar los permisos antes de eliminar
+    if (this.usuarioActual && this.usuarioActual.email === 'si@si.si') {
+      try {
+        await this.firestore.collection('/productos').doc(id).delete();
+        console.log('Producto eliminado exitosamente.');
+      } catch (error) {
+        console.error('Error al eliminar el producto:', error);
+      }
+    } else {
+      console.log('No tienes permisos para eliminar productos.');
+    }
+  }
+
 }
+ 
+
+  
+
+
+
